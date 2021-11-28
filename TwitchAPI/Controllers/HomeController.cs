96be559a7f6238a87529a1f1c2fcae334abffed3
Koci <kociandreas@seznam.cz>
2021-11-28 14:51:24 +0100

    Initiated a succesful connection to twitch API and retrieved data

---
+++ TwitchAPI/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TwitchAPI.Models;
+
+namespace TwitchAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            "https://api.twitch.tv/helix/users?login=twitchdev");
+            request.Headers.Add("Authorization", "Bearer 3dzpqshuvd6opstwfzmh69i5astiwd");
+            request.Headers.Add("Client-Id", "1uwdj9owa71a5prb3crveucdval8hp");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return View("Privacy", await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                return View("Privacy", "Something happened!");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
